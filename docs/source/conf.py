be153b7efa40b595c767308c467a4a53d83342db
Thanh Tung Khuat <thanhtung09t2@gmail.com>
2022-04-22 21:47:23 +1000

    change config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,8 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
-"""
 sys.path.append(os.path.abspath(
     os.path.join(__file__, "../../../hbbrain")
 ))
@@ -34,7 +33,6 @@ sys.path.append(os.path.abspath(
 sys.path.append(os.path.abspath(
     os.path.join(__file__, "../../../hbbrain/utils")
 ))
-"""
 
 #  on_rtd is whether we are on readthedocs.org, this line of code grabbed
 #  from docs.readthedocs.org
