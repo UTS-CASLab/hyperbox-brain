cd0b06f036bc78a9786d557b7cecca87ab4bc146
Thanh Tung Khuat <thanhtung09t2@gmail.com>
2022-04-23 11:05:14 +1000

    update config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,20 +18,20 @@ import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
 sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../../hbbrain")
+    os.path.join(__file__, "../../hbbrain")
 ))
 
 sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../../hbbrain/base")
+    os.path.join(__file__, "../../hbbrain/base")
 ))
 sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../../hbbrain/mixed_data")
+    os.path.join(__file__, "../../hbbrain/mixed_data")
 ))
 sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../../hbbrain/numerical_data")
+    os.path.join(__file__, "../../hbbrain/numerical_data")
 ))
 sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../../hbbrain/utils")
+    os.path.join(__file__, "../../hbbrain/utils")
 ))
 
 #  on_rtd is whether we are on readthedocs.org, this line of code grabbed
@@ -57,10 +57,10 @@ pygments_style = 'sphinx'
 # ones.
 extensions = [
 	'sphinx.ext.autodoc',
+	'numpydoc',
 	'sphinx.ext.duration',
     'sphinx.ext.doctest',
 	'sphinx.ext.autosummary',
-	'sphinx.ext.napoleon',
 	'sphinx.ext.autosectionlabel',
     'nbsphinx',
     'sphinx.ext.mathjax',
@@ -69,13 +69,13 @@ extensions = [
 ]
 #'sphinx.ext.linkcode',
 # 'sphinx.ext.napoleon',
-#numpydoc_class_members_toctree = False
-# 'numpydoc',
+
+numpydoc_class_members_toctree = False
 
 source_suffix = ['.rst', '.ipynb']
 
 autosummary_generate = True
-napoleon_google_docstring = False
+# napoleon_google_docstring = False
 
 # The master toctree document.
 master_doc = 'index'
