bf2cecedbf0d813c63b0dc47dd590825f8813b10
NhatQuangDo <nquang.work@gmail.com>
2025-10-19 10:50:40 +1100

    New learner category for HMR and UHMR models.

---
+++ hbbrain/numerical_data/regression_learner/hmr.py
@@ -0,0 +1,146 @@
+# Import necessary library
+import numpy as np
+import pandas as pd
+import time
+import random
+import itertools
+from hbbrain.base.base_hmr_regressor import BaseHMRegressor, is_contain_missing_value, convert_format_missing_input_zero_one
+from hbbrain.utils.membership_calc import membership_func_gfmm
+
+class HMR_learner(BaseHMRegressor):
+    def __init__ (self, theta = 0.1, _lambda = 1, V = None, W= None, d1 = None , d2 = None, r = None, N_samples=None, lambda_reg = 1e-4):
+        super().__init__( theta, _lambda, V, W, d1, d2, r)
+        self.lambda_reg = lambda_reg
+        self.N_samples = N_samples if N_samples is not None else np.array([])
+
+    def fit (self, X, y):
+        n_samples = X.shape[0]
+        if X.shape[0] > n_samples:
+            Xl = X[:n_samples, :]
+            Xu = X[n_samples:, :]
+            yl = y[:n_samples]
+            yu = y[n_samples:]
+            return self._fit(Xl, Xu, yl, yu)
+        return self._fit(X,X,y,y)
+
+    def _fit (self, Xl, Xu, yl, yu):
+        # Start
+        self._init_hyperboxes()
+        is_certain = not np.any(Xl != Xu)
+
+        if (is_contain_missing_value(Xl) == True) or (is_contain_missing_value(Xu) == True):
+            is_exist_missing_value = True
+            Xl, Xu, yl, yu = convert_format_missing_input_zero_one(
+                Xl, Xu, yl, yu,
+                np.nanmean(yl),
+                np.nanmean(yu)
+            )
+        else:
+            is_exist_missing_value = False
+        
+
+        time_start = time.perf_counter()
+        # Step 1: Hyperbox clustering
+        n_samples, n_features = Xl.shape
+        for i in range (n_samples):
+            if (self.W.size == 0 or self.V.size ==0):
+                self.W = np.array([Xu[i,:]])
+                self.V = np.array([Xl[i,:]])
+                self.N_samples = np.array([1])
+                continue
+
+            # Check top-k member ship function ( all existing hyperbox )
+            if is_exist_missing_value:
+                membership_val = membership_func_gfmm(Xl[i,:], Xu[i,:],np.minimum(self.V, self.W), np.maximum (self.W, self.V), self._lambda)
+            else: 
+                membership_val = membership_func_gfmm(Xl[i,:], Xu[i,:],self.V,self.W, self._lambda)
+            
+            ranked_idx = np.argsort(membership_val)[::-1]
+            is_expanded = False
+            for idx in ranked_idx:
+                V_new= np.minimum(self.V[idx], Xl[i,:])
+                W_new = np.maximum (self.W[idx], Xu[i,:])
+
+                # Expansion check (60%) dim:
+                if np.mean((W_new - V_new) <= self.theta) >= 0.6:
+                    self.V[idx] = V_new
+                    self.W[idx] = W_new
+                    self.N_samples[idx] += 1
+                    is_expanded = True
+                    break
+                else:
+                    continue
+
+            # Create new hyperbox if not expanded
+            if not is_expanded:
+                self.V = np.concatenate((self.V, Xl[i,:].reshape(1,-1)), axis = 0)
+                self.W = np.concatenate((self.W, Xu[i,:].reshape(1,-1)), axis = 0)
+                self.N_samples = np.append(self.N_samples, 1)
+
+        # Step 2: Least Square Optimization with OLS
+        A = []
+        n_hyperboxes = self.V.shape[0]
+        if not is_certain:
+            self.r = np.zeros((n_hyperboxes, 2))
+            self.d1 = np.zeros((n_hyperboxes, 2* n_features))
+            self.d2 = np.zeros((n_hyperboxes, 2* n_features))
+        else:
+            self.r = np.zeros(n_hyperboxes)
+            self.d1 = np.zeros((n_hyperboxes, n_features))
+            self.d2 = np.zeros((n_hyperboxes, n_features))
+
+        # Second pass for LSO
+        for i in range(n_samples):
+            if is_exist_missing_value:
+                membership_val_new = membership_func_gfmm(Xl[i,:], Xu[i,:],np.minimum(self.V, self.W), np.maximum (self.W, self.V), self._lambda)
+            else:
+                membership_val_new = membership_func_gfmm(Xl[i,:], Xu[i,:], self.V, self.W, self._lambda)
+            # Normalize
+            membership_val_norm = membership_val_new / (membership_val_new.sum() + 1e-15)
+
+            # Form X
+            if not is_certain:
+                dim_w = 2*n_features + 1
+                X = np.hstack((Xl[i,:], Xu[i,:]))
+            else:
+                dim_w = n_features + 1
+                X = Xu[i,:]
+
+            # Turn 3D -> 2D arr for matrix calculation
+            A_sample = membership_val_norm.reshape(-1,1) * np.append(X, 1).reshape (1,-1)
+            A.append(A_sample.flatten())
+
+        A = np.array(A).reshape(n_samples, -1)
+
+        reg_mask = np.ones(A.shape[1], dtype=bool)
+
+        for i in range(n_hyperboxes):
+            reg_mask[i * dim_w + dim_w - 1] = False  # skip bias term of each hyperbox
+        I = np.diag(reg_mask.astype(float))
+
+        Yl = yl.reshape(-1, 1)
+        Yu = yu.reshape(-1, 1)
+
+        if is_certain:
+            D = np.linalg.solve(A.T @ A + self.lambda_reg * I , A.T @ Yl).ravel()
+            D = D.flatten()
+            # Extract tunned paremeters for regressor
+            for i in range(n_hyperboxes):
+                base = i * dim_w
+                self.d1[i] = self.d2[i] = D[base: base + n_features]
+                self.r[i]= D[base + n_features]
+        else:
+            # Handling uncertaincy utilsising 2 regressors / hyperbox
+            A_pinv = np.linalg.solve(A.T @ A + self.lambda_reg * I, A.T)
+            D1 = (A_pinv @ Yl).ravel()
+            D2 = (A_pinv @ Yu).ravel()
+            for i in range(n_hyperboxes):
+                base = i * dim_w
+                self.d1[i] = D1[base: base + dim_w - 1]
+                self.d2[i] = D2[base: base + dim_w - 1]
+                self.r[i,0]= D1[base + dim_w - 1]
+                self.r[i,1]= D2[base + dim_w - 1]
+            
+        time_end = time.perf_counter()
+        self.elapsed_time = time_end - time_start
+        return self 
